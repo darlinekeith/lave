6cbdc258723597ad0982b2fa37669de9b2f6bc74
darlinekeith <41939469+darlinekeith@users.noreply.github.com>
2022-10-19 14:38:58 +0300

    layer1
    this is lave's firstprototype

---
+++ src/components/Blog.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {PhoneIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
+import {ChipIcon, SupportIcon} from '@heroicons/react/solid';
+
+import suppoetImg from '../assets/pexels-shelagh-murphy-2883380.jpg'
+
+const Blog = () => {
+  return (
+    <div>
+      <div>
+        <img src={suppoetImg} alt="/" />
+      </div>
+      <div>
+        <div>
+          <h2>Blog</h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
