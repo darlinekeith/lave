9126138685ef6095b39daa2c601351abdd96c94f
darlinekeith <41939469+darlinekeith@users.noreply.github.com>
2023-11-17 17:17:28 +0300

    photogallery final

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,55 +1,69 @@
+// src/components/PhotoGallery.jsx
+
 import React from 'react';
-import Carousel from './carousel';
+// Make sure to import the Carousel component with the correct case
+
+import img3 from '../assets/img3.png';
+import img5 from '../assets/img5.png';
+import img6 from '../assets/img6.png';
+import img7 from '../assets/img7.png';
+import img8 from '../assets/img8.png';
+import img9 from '../assets/img9.png';
 
 const PhotoGallery = () => {
   return (
-    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-     <div> <Carousel /> </div> 
-    <div class="-m-1 flex flex-wrap md:-m-2">
-      <div class="flex w-1/2 flex-wrap">
-        <div class="w-1/2 p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+    <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+
+      <div className="-m-1 flex flex-wrap md:-m-2">
+        <div className="flex flex-wrap w-full">
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img8}
+            />
+          </div>
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img3}
+            />
+          </div>
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img9}
+            />
+          </div>
         </div>
-        <div class="w-1/2 p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
-        </div>
-        <div class="w-full p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
-        </div>
-      </div>
-      <div class="flex w-1/2 flex-wrap">
-        <div class="w-full p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-        </div>
-        <div class="w-1/2 p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-        </div>
-        <div class="w-1/2 p-1 md:p-2">
-          <img
-            alt="gallery"
-            class="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+        <div className="flex flex-wrap w-full">
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img7}
+            />
+          </div>
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img5}
+            />
+          </div>
+          <div className="w-full p-2 md:w-1/2 lg:w-1/3">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src={img6}
+            />
+          </div>
         </div>
       </div>
     </div>
-    </div>
-    
-  )
+  );
 };
 
 export default PhotoGallery;
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,10 +1,13 @@
-// src/components/carousel.jsx
+// src/components/Carousel.jsx
 
 import React, { useState } from 'react';
+import img2 from '../assets/img2.png';
+import img1 from '../assets/img1.png';
+import img4 from '../assets/img4.png';
 
 const Carousel = () => {
   const [slide, setSlide] = useState(0);
-  const maxSlides = 3; // Set the total number of slides here
+  const maxSlides = 1; // Show only one image per click
 
   const handlePrevClick = () => {
     setSlide(Math.max(slide - 1, 0));
@@ -19,17 +22,17 @@ const Carousel = () => {
       <div className="carousel relative overflow-hidden bg-gray-300">
         <div
           className="carousel-inner flex transition-transform"
-          style={{ transform: `translateX(-${slide * 100}%)` }}
+          style={{ transform: `translateX(-${slide * 100}%)`, transition: 'transform 0.5s ease' }}
         >
           {/* Add your images here */}
-          <div className="carousel-item">
-            <img src="image1.jpg" alt="Slide 1 - Description" />
+          <div className="carousel-item w-full md:w-1/3">
+            <img src={img1} alt="Slide 1 - Description" className="w-full md:rounded-lg md:object-cover md:object-center" />
           </div>
-          <div className="carousel-item">
-            <img src="image2.jpg" alt="Slide 2 - Description" />
+          <div className="carousel-item w-full md:w-1/3">
+            <img src={img2} alt="Slide 2 - Description" className="w-full md:rounded-lg md:object-cover md:object-center" />
           </div>
-          <div className="carousel-item">
-            <img src="image3.jpg" alt="Slide 3 - Description" />
+          <div className="carousel-item w-full md:w-1/3">
+            <img src={img4} alt="Slide 3 - Description" className="w-full md:rounded-lg md:object-cover md:object-center" />
           </div>
           {/* Repeat as many times as needed */}
         </div>
@@ -37,13 +40,13 @@ const Carousel = () => {
 
       <button
         onClick={handlePrevClick}
-        className="absolute inset-y-0 left-0 z-10 p-4 opacity-50 hover:opacity-100 focus:outline-none"
+        className="absolute inset-y-0 left-0 z-10 p-4 opacity-50 hover:opacity-100 focus:outline-none hidden md:block"
       >
         Previous
       </button>
       <button
         onClick={handleNextClick}
-        className="absolute inset-y-0 right-0 z-10 p-4 opacity-50 hover:opacity-100 focus:outline-none"
+        className="absolute inset-y-0 right-0 z-10 p-4 opacity-50 hover:opacity-100 focus:outline-none hidden md:block"
       >
         Next
       </button>
